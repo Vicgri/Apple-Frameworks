3fef647c6bdf843b904025a43f1f0b3ee164daf8
Vicgri <89450050+Vicgri@users.noreply.github.com>
2023-09-26 19:45:21 +0200

    Initial Commit

---
+++ Framework-App/Framework_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Framework_AppApp.swift
+//  Framework-App
+//
+//  Created by Victoria Grimen on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Framework_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
